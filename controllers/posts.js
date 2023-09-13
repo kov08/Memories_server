5cffb88f42ff09b129804e580690e2d8fe3c22c4
kov08 <69028031+kov08@users.noreply.github.com>
2023-09-13 15:32:41 -0400

    Recomendation feacture implemented successfully (however, some problem with posts tile

--- controllers/posts.js
+++ controllers/posts.js
@@ -2,18 +2,47 @@ import mongoose from "mongoose";
 import PostMessage from "../models/postMessage.js";
 
 
-export const getPosts = async (req, res) => {
-    try {
-        const postMessages = await PostMessage.find();
+// export const getPosts = async (req, res) => {
+//     try {
+//         const postMessages = await PostMessage.find();
         
-        // console.log(postMessages);
-        res.status(200).json(postMessages);
+//         console.log(postMessages);
+//         res.status(200).json(postMessages);
+
+//     } catch (error) {
+//         res.status(404).json({ message: error.message });
+//     }
+//     // res.send('THIS WORKS ! Amazing great'); THIS CAUSE ERROR OF CONNECTION LOST!
+// } 
+
+export const getPost = async (req, res) => {
+    const { id }= req.params;
+
+    try {
+        const post = await PostMessage.findById(id);
+
+        res.status(200).json(post);        
+    } catch (error) {
+        res.status(404).json({ message: error.message });
+    }
+}
+
+export const getPosts = async (req, res) => {
+    const { page} = req.query;
+    // console.log(error.message);
+    
+    try{
+    const LIMIT = 8 ;
+    const startIndex = (Number(page) - 1) * LIMIT;  // to get starting index of every page
+    const total = await PostMessage.countDocuments({});
+    const posts = await PostMessage.find().sort({ _id: -1}).limit(LIMIT).skip(startIndex);
+    res.status(200).json({ data: posts, currentPage: Number(page), numberOfPages: Math.ceil(total / LIMIT) });
+    // res.send('THIS WORKS ! Amazing great'); THIS CAUSE ERROR OF CONNECTION LOST!
 
     } catch (error) {
         res.status(404).json({ message: error.message });
     }
-    // res.send('THIS WORKS ! Amazing great'); THIS CAUSE ERROR OF CONNECTION LOST!
-} 
+}
 
 export const getPostsBySearch = async (req, res) => {
     const {searchQuery, tags} = req.query
@@ -21,6 +50,7 @@ export const getPostsBySearch = async (req, res) => {
     try {
         const title = new RegExp(searchQuery, 'i');
         const posts = await PostMessage.find({ $or: [{title} ,{tags: { $in: tags.split(',') }}]});
+        
 
         res.json({data: posts});
     } catch (error) {
--- routes/posts.js
+++ routes/posts.js
@@ -1,11 +1,12 @@
 import express from "express";
-import {getPostsBySearch, getPosts, createPost, updatePost, deletePost, likePost } from "../controllers/posts.js";
+import {getPostsBySearch, getPost, getPosts, createPost, updatePost, deletePost, likePost } from "../controllers/posts.js";
 import auth from "../middleware/auth.js";
 
 
 const router = express.Router();
 
 router.get('/search', getPostsBySearch);
+router.get('/:id', getPost);
 router.get('/', getPosts);
 router.post('/', auth, createPost);
 router.patch('/:idTest',auth, updatePost);
